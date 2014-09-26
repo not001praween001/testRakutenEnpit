0cd2bd67bb991c6db558d7d32c4cb44f02a60907
Praween AMONTAMAVUT <not001praween001@users.noreply.github.com>
2014-09-26 11:16:35 +0900

    Update hello.rb

--- hello.rb
+++ hello.rb
@@ -16,6 +16,15 @@ get '/' do
     c.affiliate_id = ENV["AFID"]
   end
 
+  root = RakutenWebService::Ichiba::Genre.root # root genre
+  # children returns sub genres
+  root.children.each do |child|
+    puts "[#{child.id}] #{child.name}"
+  end
+   
+  # Use genre id to fetch genre object
+  RakutenWebService::Ichiba::Genre[100316].name # => "水・ソフトドリンク"
+
   # Use genre id to fetch genre object
   @rankings = RakutenWebService::Ichiba::Item.ranking(:age => 30, :sex => 0)
   erb :item_ranking
